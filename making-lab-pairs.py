14109b94e7292d5b6358297ece7b9b3496978bcb
Jennifer Meara <jennifer@jmeara.com>
2021-03-08 00:48:47 +0000

    streamlined .txt file and random choice process

--- making-lab-pairs.py
+++ making-lab-pairs.py
@@ -13,10 +13,9 @@ def create_students(student_file):
     for line in s_file:
         line = line.rstrip()
         student_info = line.split()
-        student_num = int(student_info[0])
-        student_name = str(student_info[1])
-        student_pairs = set([int(num) for num in student_info[2:]])
-        students[student_num] = (student_name, student_pairs)
+        student_name = student_info[0]
+        student_pairs = set([name for name in student_info[1:]])
+        students[student_name] = student_pairs
         
     s_file.close()
         
@@ -34,37 +33,25 @@ def create_pairs(students):
     # loop: while more than 1 item is in dict
     while len(students) > 1:
         
-        # randomly choose first student in pair & update student info dict and num list
-        first_num = 0
-        while True:
-            first_num = random.randint(1, student_count + 1)
-            if first_num not in set(students.keys()):
-                continue
-            else:
-                break    
-        first_student = students.pop(first_num)
+        # randomly choose first student in pair & update student info dict and num list        
+        first_student = students.popitem()
+        remaining_names = list(students.keys())
         
         # second student must not be the same as first, and must not be within first's previous pairs
         # randomly choose second student in pair & update student info dict and num list
-        second_num = 0
         while True:
-            second_num = random.randint(1, student_count + 1)
-            if second_num not in set(students.keys()):
-                continue
-            if (second_num == first_num) or (second_num in first_student[1]):
-                continue
-            else:
+            name = random.choice(remaining_names)
+            if name not in first_student[1]:
                 break
-        second_student = students.pop(second_num)
+        prev_pairs = students.pop(name)
+        second_student = (name, prev_pairs)
         
-        # add student and partner names to today's pairs (nested lists? or nested tuples?)
-        todays_pairs.append([first_student[0], second_student[0]])
+        todays_pairs.append([first_student, second_student])
 
     # if odd number, add leftover name
     if students:
-        last_num = list(students.keys())[0]
-        last_student = students.pop(last_num)
-        todays_pairs.append([last_student[0]])
+        last_student = students.popitem()
+        todays_pairs.append([last_student])
     
     # return pair list
     return(todays_pairs)
@@ -78,11 +65,14 @@ print()
 
 todays_pairs = create_pairs(students)
 print('**** STUDENT PAIRS SUCCESS!!!! ****')
-
-
+print()
 
 # loop through pairs and print assignment to labs Malala a - g and ruth a - g
-# for pair in todays_pairs:
-#     print(pair)
+for pair in todays_pairs:
+    if len(pair) == 2:
+        print(pair[0][0], pair[1][0])
+    else:
+        print(pair[0][0])
+
 
 
